63c0a91f6318728cee23f1be4b5327a4a02fae36
kledi mama <kledi37@gmail.com>
2022-02-21 22:47:45 -0500

    essage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { createTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
+import {Typography} from '@material-ui/core';
+import NavBar from './components/NavBar';
+import Grid from './components/Grid';
+import Footer from './components/Footer';
+import DateTimePicker from 'react-datetime-picker'
+
+import EventNoteIcon from '@material-ui/icons/EventNote';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main:"#2e1667",
+    },
+    secondary: {
+      main:"#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+      },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+});
+
+const styles = makeStyles({
+  wrapper: {
+    width: "65%",
+    margin: "auto",
+    textAlign: "center"
+  },
+  bigSpace: {
+    marginTop: "5rem"
+  },
+  littleSpace:{
+    marginTop: "2.5rem",
+  },
+  grid:{
+    display: "flex", 
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap", 
+  },
+})
+
+
 
 function App() {
+  const [value, onChange] = useState(new Date());
+  const [nHour,hourSelect] = useState(0);
+  const classes = styles();
+  const currentDate=new Date();
+
+
+  function calculatePrice(date,hours){
+   // console.log("Date",date)
+  if ((date.getDay()===6) || (date.getDay()===0)){
+    return 150*hours
+  }
+  else{
+    return 100*hours
+  } 
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+      <NavBar></NavBar>
+      <div>
+      <div className={classes.wrapper}>
+          <Typography variant="h4" className={classes.bigSpace} color="primary">
+             Booking App By Kledi Mama
+          </Typography>
+          <Typography variant="h5" className={classes.littleSpace} color="primary">
+          Here at BookingByKledi we strive to offer our clients the most reliable and efficient booking service available. It is without a doubt the most state of the art and easy to use service on the market. This is because we value our customers deeply and believe that our clients deserve nothing but the best.To continue, simply select the date and amount of time required for the booking.
+          </Typography>
+        </div>
+      <div className={`${classes.grid} ${classes.smallSpace}`}>          
+          <grid>
+            <div>
+              <p>
+              <strong>PLEASE SELECT YOUR DESIRED DATE</strong>
+              </p>
+            </div>
+            <DateTimePicker onChange={onChange} value={value} 
+            minDate={currentDate}/>
+            <div>
+              <p>
+            <strong>NUMBER OF HOURS REQUIRED:</strong>
+            </p>
+            </div>
+            <div>
+            <input type="number"  min="1" max="12" onChange={e => hourSelect(e.target.value)} ></input>
+            </div>
+          </grid>
+          <Grid icon={<EventNoteIcon style={{fill: "#7e4dee", height:"125", width:"125"}}/>} title={calculatePrice(value,nHour)} text="Your Price Estimation In $:" btnTitle="Proceed With Booking"/>
+        </div>
+        </div>
+        <div className={classes.bigSpace}>
+          <Footer/>
+        </div>
+      </ThemeProvider>
     </div>
   );
 }
